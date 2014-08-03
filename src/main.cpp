420b7621e8086bb39d79153bb70954a92ed7c415
StepS <steps@worms2d.info>
2014-08-04 02:44:29 +0400

    Use msvcrt.lib to minimize the size of the DLL

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,6 @@
 #include <Windows.h>
 #include "init.h"
 
-#ifdef STATICHOOKS
-#pragma comment (lib, "MinHook\\libMinHook.x86.lib")
-#else
-#pragma comment (lib, "MinHook\\MinHook.x86.lib")
-#endif
-
 BOOL APIENTRY DllMain(HMODULE, DWORD ul_reason_for_call, LPVOID)
 {
 	if (ul_reason_for_call == DLL_PROCESS_ATTACH)
